d903cab7b0a35b20b8bae1e5193a2ebaa0d617ee
MAJJIGA SAI SWETHA SREE <saiswethasree14@gmail.com>
2024-08-01 20:36:20 +0530

    Add files via upload

---
+++ Springnormalxml/src/demo/Demo1.java
@@ -0,0 +1,10 @@
+package demo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		String pat = "demo/first";
+		ApplicationContext con = new classpathxmlApplicationContext(pat);
+	}
+
+}
